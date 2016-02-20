14ad4ff2330632987891b0a7a3d7153428f87171
kkorolyov <kirillk753@gmail.com>
2016-02-19 21:36:16 -0800

    Created Results object to wrap ResultSet. Requires testing.

--- src/dev/kkorolyov/ezdb/construct/Results.java
+++ src/dev/kkorolyov/ezdb/construct/Results.java
@@ -3,9 +3,9 @@ package dev.kkorolyov.ezdb.construct;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
 
+import dev.kkorolyov.ezdb.exceptions.ClosedException;
+import dev.kkorolyov.ezdb.exceptions.MismatchedTypeException;
 import dev.kkorolyov.ezdb.logging.DebugLogger;
 
 /**
@@ -15,12 +15,11 @@ import dev.kkorolyov.ezdb.logging.DebugLogger;
  * @see ResultSet
  * @see RowEntry
  */
-public class Results {
+public class Results implements AutoCloseable {
 	private static final DebugLogger log = DebugLogger.getLogger(Results.class.getName());
 	
 	private ResultSet rs;
-	private List<RowEntry[]> rowEntries = new LinkedList<>();
-	private int cursor = -1;	// Directly before first row
+	private Column[] columns;
 	
 	/**
 	 * Constructs a new {@code Results} object from a {@code ResultSet}.
@@ -31,36 +30,124 @@ public class Results {
 	}
 	
 	/**
+	 * Closes this results object and releases all resources.
+	 * Does nothing if called on a closed results.
+	 */
+	public void close() {
+		if (isClosed())
+			return;
+		
+		try {
+			rs.close();
+			rs = null;
+			columns = null;
+		} catch (SQLException e) {
+			log.exceptionSevere(e);
+		}
+	}
+	/** @return	{@code true} if this resource is closed, {@code false} if otherwise */
+	public boolean isClosed() {
+		return (rs == null);
+	}
+	
+	/**
 	 * Retrieves the column information (name and type) associated with these results.
 	 * @return all columns
+	 * @throws ClosedException if called on a closed resource
 	 */
-	public Column[] getColumns() {
+	public Column[] getColumns() throws ClosedException {
+		if (isClosed())
+			throw new ClosedException();
 		
+		if (columns == null) {	// Not yet cached
+			try {
+				ResultSetMetaData rsmd = rs.getMetaData();
+				
+				columns = new Column[rsmd.getColumnCount()];
+				for (int i = 0; i < columns.length; i++) {
+					int rsmdColumn = i + 1;	// ResultSet columns start from 1
+					
+					String columnName = rsmd.getColumnName(rsmdColumn);
+					SqlType columnType = SqlType.get(rsmd.getColumnType(rsmdColumn));
+					
+					columns[i] = new Column(columnName, columnType);
+				}
+			} catch (SQLException e) {
+				log.exceptionSevere(e);
+			}
+		}
+		return columns;
+	}
+	
+	/** 
+	 * Returns the number of columns in this results.
+	 * @return total number of columns
+	 * @throws ClosedException if called on a closed resource
+	 */
+	public int getNumColumns() throws ClosedException {
+		if (isClosed())
+			throw new ClosedException();
+		
+		return getColumns().length;
 	}
 	
 	/**
 	 * Retrieves the next row of results.
 	 * @return next row of results, or {@code null} if no more rows
+	 * @throws ClosedException if called on a closed resource
 	 */
-	public RowEntry[] getNextRow() {
-		
-	}
-	public RowEntry[] getPreviousRow() {
-		
-	}
-	
-	
-	/** @return total number of columns */
-	public int getNumColumns() {
-		int numColumns = -1;
+	public RowEntry[] getNextRow() throws ClosedException {
+		if (isClosed())
+			throw new ClosedException();
+			
+		RowEntry[] row = null;
 		try {
-			numColumns = rs.getMetaData().getColumnCount();
-		} catch (SQLException e) {
-			e.printStackTrace();
+			if (rs.next()) {
+				row = new RowEntry[getNumColumns()];
+				
+				for (int i = 0; i < row.length; i++) {
+					row[i] = new RowEntry(getColumns()[i], setValue(rs, i));
+				}
+			}
+		} catch (SQLException | MismatchedTypeException e) {
+			log.exceptionSevere(e);
 		}
-		return numColumns;
+		return row;
 	}
-	public int getNumRows() {
+	private Object setValue(ResultSet rs, int columnIndex) throws ClosedException {
+		Object value = null;
+		int rsIndex = columnIndex + 1;	// ResultSet index starts 1 ahead of column
 		
+		try {
+			switch (getColumns()[columnIndex].getType()) {
+			case BOOLEAN:
+				value = rs.getBoolean(rsIndex);
+				break;
+			case SMALLINT:
+				value = rs.getShort(rsIndex);
+				break;
+			case INTEGER:
+				value = rs.getInt(rsIndex);
+				break;
+			case BIGINT:
+				value = rs.getLong(rsIndex);
+				break;
+			case REAL:
+				value = rs.getFloat(rsIndex);
+				break;
+			case DOUBLE:
+				value = rs.getDouble(rsIndex);
+				break;
+			case CHAR:
+				value = rs.getString(rsIndex).charAt(0);
+				break;
+			case VARCHAR:
+				value = rs.getString(rsIndex);
+				break;
+			}
+		} catch (SQLException e) {
+			log.exceptionSevere(e);
+		}
+		return value;
 	}
 }
---
+++ test/dev/kkorolyov/ezdb/construct/ResultsTest.java
@@ -0,0 +1,50 @@
+package dev.kkorolyov.ezdb.construct;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class ResultsTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsClosed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColumns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumColumns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextRow() {
+		fail("Not yet implemented");
+	}
+
+}
