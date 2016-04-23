706d0a797be11ab6c4bb504add8557bf2f093d9b
kkorolyov <kirillk753@gmail.com>
2016-04-22 21:06:51 -0700

    Add suite test for connections

---
+++ test/dev/kkorolyov/sqlob/connection/concrete/ConnectionTests.java
@@ -0,0 +1,15 @@
+package dev.kkorolyov.sqlob.connection.concrete;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuppressWarnings("javadoc")
+@RunWith(Suite.class)
+
+@SuiteClasses({ SimpleDatabaseConnectionTest.class,
+								SimpleTableConnectionTest.class})
+
+public class ConnectionTests {
+	// Suite
+}
