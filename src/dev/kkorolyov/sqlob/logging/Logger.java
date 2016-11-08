80fea4956ce834639a84b9bb11214336da95a1e9
kkorolyov <kirillk753@gmail.com>
2016-11-07 19:51:42 -0800

    Violently refactor project

--- src/dev/kkorolyov/sqlob/logging/Logger.java
+++ src/dev/kkorolyov/sqlob/logging/Logger.java
@@ -1,33 +1,23 @@
 package dev.kkorolyov.sqlob.logging;
 
 /**
- * Provides the appropriate logger or a stub depending on the existence of the {@code SimpleLogs} API during runtime.
+ * Provides the appropriate logger or a stub depending on the existence of the {@code SimpleLogs} library during runtime.
  */
 public class Logger {
-	private static final boolean SIMPLE_LOGS_FOUND;
+	private static boolean simpleLogsFound = true;
 	private static LoggerInterface stub;
-		
-	static {
-		boolean simpleLogsFound = true;
-		try {
-			Class.forName("dev.kkorolyov.simplelogs.Logger");
-		} catch (ClassNotFoundException e) {
-			simpleLogsFound = false;
-		}
-		SIMPLE_LOGS_FOUND = simpleLogsFound;
-	}
 	
 	/**
 	 * Returns the appropriate logger for a specified name or a stub if the {@code SimpleLogs} API is not found.
 	 * @param name name of logger to get
 	 * @return appropriate logger
 	 */
-	public static LoggerInterface getLogger(String name) {		
-		if (SIMPLE_LOGS_FOUND) {
+	public static LoggerInterface getLogger(String name) {
+		if (simpleLogsFound) {
 			try {
 				return (LoggerInterface) Class.forName("dev.kkorolyov.sqlob.logging.LoggerImplementation").getDeclaredConstructor(new Class[]{String.class}).newInstance(name);
 			} catch (Exception e) {
-				e.printStackTrace();
+				simpleLogsFound = false;	// SimpleLogs not found
 			}
 		}
 		if (stub == null)
