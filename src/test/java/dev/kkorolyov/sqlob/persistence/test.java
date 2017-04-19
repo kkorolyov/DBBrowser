44cf4f3e831040c65af4bbeabf0c9df80dcc83c5
kkorolyov <kirillk753@gmail.com>
2017-04-18 20:17:14 -0700

    Move to Gradle

---
+++ src/test/java/dev/kkorolyov/sqlob/persistence/test.java
@@ -0,0 +1,19 @@
+package dev.kkorolyov.sqlob.persistence;
+
+import java.sql.SQLException;
+
+import dev.kkorolyov.sqlob.JDBCMocks;
+import dev.kkorolyov.sqlob.Stub.BasicStub;
+
+/**
+ *
+ */
+public class test {
+	public static void main(String[] args) throws SQLException {
+		JDBCMocks mocks = new JDBCMocks();
+		while (true) {
+			Session session = new Session(mocks.ds);
+			session.put(BasicStub.random());
+		}
+	}
+}
