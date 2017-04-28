dea35fbdbf27c4d4a24bc463f3d9ad289b8aa544
kkorolyov <kirillk753@gmail.com>
2017-04-27 20:23:33 -0700

    Update StatementGeneratorSpec

--- src/test/java/dev/kkorolyov/sqlob/service/StatementGeneratorSpec.groovy
+++ src/test/java/dev/kkorolyov/sqlob/service/StatementGeneratorSpec.groovy
@@ -10,13 +10,13 @@ import java.lang.reflect.Field
 import static dev.kkorolyov.sqlob.service.Constants.ID_NAME
 
 class StatementGeneratorSpec extends Specification {
-  static {
-    Logger.getLogger("dev.kkorolyov.sqlob", Logger.Level.DEBUG, new PrintWriter(System.err));	// Enable logging
-  }
-
   Mapper mapper = Mock()
 
   StatementGenerator generator = new StatementGenerator(mapper)
+
+	def setupSpec() {
+		Logger.getLogger("dev.kkorolyov.sqlob", Logger.Level.DEBUG, new PrintWriter(System.err));	// Enable logging
+	}
 
   def "uses mapped names"() {
 		Class c = new Object() {
@@ -25,10 +25,10 @@ class StatementGeneratorSpec extends Specification {
 		Field f = c.getDeclaredField("field")
 		Condition where = new Condition()
 
-		when:
 		mapper.getAssociatedClasses(c) >> [c]
 		mapper.getPersistableFields(c) >> [f]
 
+		when:
 		generator.create(c)
 		generator.select(c, where)
 		generator.insert(c)
@@ -40,7 +40,7 @@ class StatementGeneratorSpec extends Specification {
 		3 * mapper.getName(f) >> "FakeField"
 	}
 
-	def "create statements create each associated class"() {
+	def "create() creates each associated class"() {
 		Object cInner1 = new Object() {}
 		Object cInner2 = new Object() {}
 		Class c = new Object() {
@@ -48,7 +48,6 @@ class StatementGeneratorSpec extends Specification {
 			Object o2 = cInner2
 		}.class
 
-		when:
 		1 * mapper.getName(cInner1.class) >> cInner1.class.getName()
 		1 * mapper.getName(cInner2.class) >> cInner2.class.getName()
 		1 * mapper.getName(c) >> c.getName()
@@ -60,16 +59,65 @@ class StatementGeneratorSpec extends Specification {
 																					 cInner2.class,
 																					 c]
 
+		expect:
 		Iterable statements = generator.create(c)
 
-		then:
 		statements.size() == 3
 		statements[0].contains(cInner1.class.getName())
 		statements[1].contains(cInner2.class.getName())
 		statements[2].contains(c.getName())
 	}
 
-	def "generateInsert() returns statement for class and per complex field"() {
+	def "select() selects all columns"() {
+		Class c = new Object() {}.class
+
+		expect:
+		generator.select(c, null).contains("*")
+	}
+	def "selectId() selects only ID column"() {
+		Class c = new Object() {}.class
+
+		expect:
+		generator.selectId(c, null).contains(ID_NAME)
+	}
+
+	def "select() adds WHERE clause if condition specified"() {
+		Class c = new Object() {}.class
+		Condition where = new Condition("FakeColumn", "=", null)
+
+		String whereClause = "WHERE " + where.toString()
+
+		expect:
+		!generator.select(c, null).contains(whereClause)
+		generator.select(c, where).contains(whereClause)
+	}
+
+	def "insert() inserts ID and persistable fields"() {
+		Class c = new Object() {
+			private String s
+			@PackageScope int i
+			boolean b
+			UUID u
+		}.class
+
+		1 * mapper.getPersistableFields(c) >> [c.getDeclaredField("s"),
+																					 c.getDeclaredField("i"),
+																					 c.getDeclaredField("b"),
+																					 c.getDeclaredField("u")]
+		4 * mapper.getName(_ as Field) >> _.getName()
+
+		expect:
+		String statement = generator.insert(c)
+
+		statement.count("=") == 5
+		statement.contains(ID_NAME)
+		statement.contains("s")
+		statement.contains("i")
+		statement.contains("b")
+		statement.contains("u")
+	}
+
+	def "insert() returns statement for class and per complex field"() {
     expect:
     generator.insert(c).size() == size
 
