9f425195ff474300842428d6a0232a8d2d8c9744
kkorolyov <kirillk753@gmail.com>
2017-04-29 01:48:21 -0700

    Add some Executor tests

--- src/test/java/dev/kkorolyov/sqlob/service/StatementExecutorSpec.groovy
+++ src/test/java/dev/kkorolyov/sqlob/service/StatementExecutorSpec.groovy
@@ -1,39 +1,75 @@
 package dev.kkorolyov.sqlob.service
 
-import spock.lang.Shared
 import spock.lang.Specification
 
+import java.lang.reflect.Field
 import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 import java.sql.Statement
 
 class StatementExecutorSpec extends Specification {
-  @Shared Mapper mapper = Mock()
-	Statement statement = Mock()
+  Mapper mapper = Mock()
+	StatementGenerator generator = Mock()
+
+	ResultSet rs = Mock()
+	Statement s = Mock()
+	PreparedStatement ps = Mock()
 	Connection conn = Mock()
 
 	StatementExecutor executor = new StatementExecutor(mapper)
 
 	def setup() {
+		rs.getObject(_ as String) >> null
+		s.executeQuery(_) >> rs
+		ps.executeQuery() >> rs
+		conn.createStatement() >> s
+		conn.prepareStatement(_) >> ps
+
+		Field generatorField = executor.class.getDeclaredField("generator")
+		generatorField.setAccessible(true)
+		generatorField.set(executor, generator)
+
 		executor.setConnection(conn)
 	}
 
-	def "create() batch executes all statements in order"() {
-		1 * conn.createStatement() >> statement
+	def "create() creates as a batch"() {
+		Class c = new Object() {}.class
+		Iterable<String> creates = ["1", "2", "3", "4"]
 
-		Iterable<String> creates = ["0", "1", "2", "3"]
+		1 * generator.create(c) >> creates
 
 		when:
-		executor.create(creates)
+		executor.create(c)
 
 		then:
-		1 * statement.addBatch(creates[0])
+		creates.each {
+			1 * s.addBatch(it)
+		}
+		1 * s.executeBatch()
+	}
+
+	def "setting new Connection commits current Connection"() {
+		when:
+		executor.setConnection(Mock(Connection))
+
 		then:
-		1 * statement.addBatch(creates[1])
+		1 * conn.commit()
+	}
+
+	def "flush() commits underlying Connection"() {
+		when:
+		executor.flush()
+
 		then:
-		1 * statement.addBatch(creates[2])
+		1 * conn.commit()
+	}
+	def "close() commits before closing"() {
+		when:
+		executor.close()
+
 		then:
-		1 * statement.addBatch(creates[3])
-		then:
-		1 * statement.executeBatch()
+		executor.isClosed()
+		1 * conn.commit()
 	}
 }
